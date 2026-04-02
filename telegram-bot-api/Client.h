3bf5ded6ed386a6cb9c60fe4b9bf0377bd1f1de6
levlam <levlam@telegram.org>
2026-04-02 17:54:09 +0300

    Add "managed_bot" updates.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -243,6 +243,7 @@ class Client final : public WebhookActor::Callback {
   class JsonUsersShared;
   class JsonChatShared;
   class JsonManagedBotCreated;
+  class JsonManagedBotUpdated;
   class JsonGiveawayCreated;
   class JsonGiveaway;
   class JsonGiveawayWinners;
@@ -1443,6 +1444,8 @@ class Client final : public WebhookActor::Callback {
 
   void add_update_purchased_paid_media(object_ptr<td_api::updatePaidMediaPurchased> &&query);
 
+  void add_update_managed_bot(object_ptr<td_api::updateManagedBot> &&query);
+
   void add_new_custom_event(object_ptr<td_api::updateNewCustomEvent> &&event);
 
   void add_new_custom_query(object_ptr<td_api::updateNewCustomQuery> &&query);
@@ -1488,6 +1491,7 @@ class Client final : public WebhookActor::Callback {
     EditedBusinessMessage,
     BusinessMessagesDeleted,
     PurchasedPaidMedia,
+    ManagedBot,
     Size
   };
 
