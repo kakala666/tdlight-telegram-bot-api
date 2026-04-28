cc469999bdd6604869395157a85e2aa1aa4dbbf0
levlam <levlam@telegram.org>
2026-04-28 17:56:42 +0300

    Add and use json_store_message_sender.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -1348,6 +1348,10 @@ class Client final : public WebhookActor::Callback {
 
   void remove_replies_to_message(int64 chat_id, int64 reply_to_message_id, bool only_from_cache);
 
+  void json_store_message_sender(td::JsonObjectScope &object, const object_ptr<td_api::MessageSender> &sender,
+                                 td::Slice user_field_name, td::Slice chat_field_name,
+                                 int64 backward_compatibility_user_id = 0) const;
+
   td::unique_ptr<MessageInfo> delete_message(int64 chat_id, int64 message_id, bool only_from_cache);
 
   void add_new_message(object_ptr<td_api::message> &&message, bool is_edited);
