bde5384c31ef5723a35b4d65e4b66ab5ea3c3186
levlam <levlam@telegram.org>
2026-05-27 14:44:55 +0300

    Add PollMedia.link.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -128,6 +128,7 @@ class Client final : public WebhookActor::Callback {
   class JsonAudio;
   class JsonAudios;
   class JsonDocument;
+  class JsonLink;
   class JsonPhotoSize;
   class JsonPhoto;
   class JsonLivePhoto;
