5298d5599ee01f25588ffa2682d2793cd6715ec8
levlam <levlam@telegram.org>
2026-06-09 10:15:29 +0300

    Add JsonRichBlockCaption.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -168,6 +168,7 @@ class Client final : public WebhookActor::Callback {
   class JsonEntity;
   class JsonVectorEntities;
   class JsonRichText;
+  class JsonRichBlockCaption;
   class JsonWebAppInfo;
   class JsonCopyTextButton;
   class JsonInlineKeyboardButton;
