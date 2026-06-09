bdbc18829971afe7f7c3286b0733ed582e669ecd
levlam <levlam@telegram.org>
2026-06-09 10:37:25 +0300

    Add JsonBlockTableCell.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -168,6 +168,7 @@ class Client final : public WebhookActor::Callback {
   class JsonEntity;
   class JsonVectorEntities;
   class JsonRichText;
+  class JsonRichTableCell;
   class JsonRichBlockCaption;
   class JsonWebAppInfo;
   class JsonCopyTextButton;
